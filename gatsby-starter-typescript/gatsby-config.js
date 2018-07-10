b75091807f60ca3832f5262dc1633d7e5009f523
Unicar <weijia@baseup.com.au>
2018-07-10 14:28:36 +1000

    Added gatsby-transformer-remark

--- gatsby-starter-typescript/gatsby-config.js
+++ gatsby-starter-typescript/gatsby-config.js
@@ -11,11 +11,19 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         name: "img",
         path: `${__dirname}/src/images`
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`
   ],
 }
---
+++ gatsby-starter-typescript/src/pages/post.md
@@ -0,0 +1,12 @@
+---
+title: "Welcome to the new blog"
+date: "2018-02-21"
+---
+
+## Hello
+
+* This
+* Is
+* A
+* New
+* Post
