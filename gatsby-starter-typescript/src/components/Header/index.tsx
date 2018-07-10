63d9b8d858ceb95bc0998a65cb1b0d9940c4da42
Unicar <weijia@baseup.com.au>
2018-07-10 10:09:58 +1000

    Moved header to components

---
+++ gatsby-starter-typescript/src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import Link from 'gatsby-link'
+
+export interface HeaderProps {
+}
+
+export default class Header extends React.Component<HeaderProps, any> {
+  public render() {
+    return (
+        <div
+        style={{
+          background: 'rebeccapurple',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
+          }}
+        >
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              Gatsby
+            </Link>
+          </h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
--- gatsby-starter-typescript/src/layouts/index.tsx
+++ gatsby-starter-typescript/src/layouts/index.tsx
@@ -1,47 +1,8 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-)
+import Header from '../components/Header'
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
